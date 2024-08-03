ac5dc2abf271fd299ceea44fffa87be9228b7c3b
Mohd Akhshan Qamar <137386287+iamak-56@users.noreply.github.com>
2024-08-03 09:21:33 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all Js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs, not brower
+
+console.log(3 + 3);
+
+console.log("akhshan");
+//  code is readability 
+
+console.log(3
+    +
+    3 )
+
+    console.log("akhshan")
+// code is unreadability
+
+
+let name = "Akhshan"
+let age = 20
+let isloggedIn = false
+
+// number => 2 to power 53'
+// bignit
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+ 
