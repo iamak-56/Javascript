ffbf5a92901c0a05b95a582e29d82fad74ed8518
Mohd Akhshan Qamar <137386287+iamak-56@users.noreply.github.com>
2024-08-04 09:42:13 +0000

    conversion od datatype

---
+++ basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInMumber = Number(score)
+console.log(typeof valueInMumber);
+console.log(valueInMumber);
+
+
+// "33" => 33
+// "33abc" =>NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Akhshan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true;
+// 0 => false;
+// "" => false;
+// "Akhshan"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
