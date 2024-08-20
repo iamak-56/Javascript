7952f531f419bfd3cf3ca4621c7515c1343b447e
akshan <mohdakhshanqamar@gmail.com>
2024-08-20 12:08:54 +0530

    test.js

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,8 @@
-console.log("Akhshan")
+const accountId = 144553
+let accountEmail = "akhshan@gmail.com"
+var accountPassward = "12345"
+accountCity = "Budaun"
+
+// accountId = 2 // not allowed
+
+console.log(accountId);
